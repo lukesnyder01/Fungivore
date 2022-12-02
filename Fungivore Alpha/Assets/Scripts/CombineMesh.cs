2c8b9b9bc56be9a9dd7b25d98c271c48ea1041e6
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2022-12-01 20:18:57 -0800

    Added pillar fields and factored out CombineMesh class

---
+++ Fungivore Alpha/Assets/Scripts/CombineMesh.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombineMesh : MonoBehaviour
+{
+    [Header("Mesh Combine Settings")]
+    public bool destroyChildren = true;
+    public bool meshIsLarge = true;
+
+
+    public void Combine(GameObject meshesToCombine)
+    {
+        Quaternion oldRotation = transform.rotation;
+        Vector3 oldPosition = transform.position;
+        Vector3 oldScale = transform.localScale;
+
+        transform.rotation = Quaternion.identity;
+        transform.position = Vector3.zero;
+        transform.localScale = Vector3.one;
+
+
+        MeshFilter[] meshFilters = meshesToCombine.GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+
+        Destroy(meshesToCombine.gameObject.GetComponent<MeshCollider>());
+
+        //Matrix4x4 myTransform = meshesToCombine.transform.worldToLocalMatrix;
+
+        for (int m = 0; m < meshFilters.Length; m++)
+        {
+            if (meshFilters[m].sharedMesh == null) continue;
+
+            combine[m].mesh = meshFilters[m].sharedMesh;
+
+            //combine[m].transform = myTransform * meshFilters[m].transform.localToWorldMatrix;
+
+            combine[m].transform = meshFilters[m].transform.localToWorldMatrix;
+            meshFilters[m].gameObject.SetActive(false);
+        }
+
+
+        var meshFilter = meshesToCombine.GetComponent<MeshFilter>();
+
+
+
+
+        meshFilter.mesh = new Mesh();
+
+        if (meshIsLarge)
+        {
+            meshFilter.mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        }
+
+        meshFilter.mesh.CombineMeshes(combine, true);
+        meshFilter.mesh.RecalculateBounds();
+        meshFilter.mesh.RecalculateNormals();
+        meshFilter.mesh.Optimize();
+
+        Mesh newMeshCollider = new Mesh();
+
+        newMeshCollider.indexFormat = meshFilter.mesh.indexFormat;
+        newMeshCollider.vertices = meshFilter.mesh.vertices;
+        newMeshCollider.triangles = meshFilter.mesh.triangles;
+
+        MeshCollider meshCollider = meshesToCombine.gameObject.AddComponent<MeshCollider>();
+
+        meshCollider.sharedMesh = newMeshCollider;
+
+        if (!destroyChildren)
+        {
+            meshesToCombine.gameObject.transform.DetachChildren();
+        }
+        else
+        {
+            foreach (Transform child in meshesToCombine.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+        }
+
+
+
+        meshesToCombine.gameObject.SetActive(true);
+
+        transform.rotation = oldRotation;
+        transform.position = oldPosition;
+        transform.localScale = oldScale;
+
+    }
+
+
+}
---
+++ Fungivore Alpha/Assets/SpawnPillarField.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CombineMesh))]
+public class SpawnPillarField : MonoBehaviour
+{
+    CombineMesh combineMesh;
+
+    public GameObject pillar;
+    public GameObject pillarFieldMeshes;
+
+    public float pillarSpawnChance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        combineMesh = GetComponent<CombineMesh>();
+
+
+        int xSize = Mathf.RoundToInt(transform.localScale.x - 7);
+        int zSize = Mathf.RoundToInt(transform.localScale.z - 7);
+
+        Vector3 pos = transform.position;
+
+        //start at upper left hand corner -1/2xSize, -1/2zSize
+        int xOffset = Mathf.RoundToInt(pos.x - ((xSize - 1) / 2));
+        int zOffset = Mathf.RoundToInt(pos.z - ((zSize - 1) / 2));
+
+       
+        for (int i = 0; i < xSize; i++)
+        {
+            for (int j = 0; j < zSize; j++)
+            {
+                if (Random.Range(0f, 100f) < pillarSpawnChance)
+                {
+                    Vector3 spawnPos = new Vector3(xOffset + i, pos.y + 0.5f, zOffset + j);
+
+                    int pillarHeight = Random.Range(10, 100);
+                    int pillarWidth = Random.Range(0, 2) * 2 + 1;
+                    Vector3 pillarScale = new Vector3(pillarWidth, pillarHeight, pillarWidth);
+
+                    GameObject newPillar = Instantiate(pillar, spawnPos, Quaternion.identity);
+
+                    newPillar.transform.localScale = pillarScale;
+
+                    newPillar.transform.parent = pillarFieldMeshes.transform;
+                }
+            }
+        }
+
+        combineMesh.Combine(pillarFieldMeshes);
+
+    }
+
+
+
+
+
+}
