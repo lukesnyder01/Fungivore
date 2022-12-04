ca585ab38e977af4ec0e1b711c441f7b3670be20
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2022-12-04 11:58:28 -0800

    Made pillar field spawning into a coroutine

--- Fungivore Alpha/Assets/SpawnPillarField.cs
+++ Fungivore Alpha/Assets/SpawnPillarField.cs
@@ -11,7 +11,25 @@ public class SpawnPillarField : MonoBehaviour
     public GameObject pillar;
     public GameObject pillarFieldMeshes;
 
+    public float waitTime = 0.01f;
+
+
+
     public float pillarSpawnChance;
+
+    private Vector3 pos;
+
+    private int xSize;
+    private int zSize;
+
+    private int xOffset;
+    private int zOffset;
+
+
+
+    public int spawnsPerWait = 10;
+    private int spawnsSinceLastWait = 0;
+
 
 
     // Start is called before the first frame update
@@ -19,27 +37,37 @@ public class SpawnPillarField : MonoBehaviour
     {
         combineMesh = GetComponent<CombineMesh>();
 
+        xSize = Mathf.RoundToInt(transform.localScale.x - 2);
+        zSize = Mathf.RoundToInt(transform.localScale.z - 2);
 
-        int xSize = Mathf.RoundToInt(transform.localScale.x - 7);
-        int zSize = Mathf.RoundToInt(transform.localScale.z - 7);
-
-        Vector3 pos = transform.position;
+        pos = transform.position;
 
         //start at upper left hand corner -1/2xSize, -1/2zSize
-        int xOffset = Mathf.RoundToInt(pos.x - ((xSize - 1) / 2));
-        int zOffset = Mathf.RoundToInt(pos.z - ((zSize - 1) / 2));
+        xOffset = Mathf.RoundToInt(pos.x - ((xSize - 1) / 2));
+        zOffset = Mathf.RoundToInt(pos.z - ((zSize - 1) / 2));
 
-       
+        StartCoroutine(SpawnPillars());
+    }
+
+
+    public IEnumerator SpawnPillars()
+    {
         for (int i = 0; i < xSize; i++)
         {
             for (int j = 0; j < zSize; j++)
             {
+                Vector3 spawnPos = new Vector3(xOffset + i, pos.y + 0.5f, zOffset + j);
+
+                Random.InitState(spawnPos.GetHashCode() % 1000);
+
                 if (Random.Range(0f, 100f) < pillarSpawnChance)
                 {
-                    Vector3 spawnPos = new Vector3(xOffset + i, pos.y + 0.5f, zOffset + j);
-
+                    Random.InitState(spawnPos.GetHashCode() % 1000);
                     int pillarHeight = Random.Range(10, 100);
+
+                    Random.InitState(spawnPos.GetHashCode() % 1000);
                     int pillarWidth = Random.Range(0, 2) * 2 + 1;
+
                     Vector3 pillarScale = new Vector3(pillarWidth, pillarHeight, pillarWidth);
 
                     GameObject newPillar = Instantiate(pillar, spawnPos, Quaternion.identity);
@@ -47,16 +75,23 @@ public class SpawnPillarField : MonoBehaviour
                     newPillar.transform.localScale = pillarScale;
 
                     newPillar.transform.parent = pillarFieldMeshes.transform;
+
+                    spawnsSinceLastWait++;
+
+                    if (spawnsSinceLastWait >= spawnsPerWait)
+                    {
+                        spawnsSinceLastWait = 0;
+                        yield return new WaitForSeconds(waitTime);
+                    }
+
+                    
                 }
+
             }
         }
 
         combineMesh.Combine(pillarFieldMeshes);
 
     }
-
-
-
-
 
 }
