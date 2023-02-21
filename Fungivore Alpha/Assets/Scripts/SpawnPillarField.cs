0a09355646059fbcfa3c64449b96fe8c570cecd7
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2023-02-21 05:05:00 -0800

    Gave pillar fields an option to spawn the pillars downwards, and refactored the pillar field spawning script

--- Fungivore Alpha/Assets/Scripts/SpawnPillarField.cs
+++ Fungivore Alpha/Assets/Scripts/SpawnPillarField.cs
@@ -13,14 +13,21 @@ public class SpawnPillarField : MonoBehaviour
 
     public float waitTime = 0.01f;
 
-    public int minHeight = 2;
+    public int minHeight = 5;
     public int maxHeight = 100;
 
     public float chanceOfDoubleHeightPillar;
 
     public float pillarSpawnChance;
 
-    private Vector3 pos;
+    public bool pillarsSpawnDownward = true;
+
+
+
+    private Vector3 boxCastDir;
+    private Vector3 halfExtents;
+
+    private Vector3 basePosition;
 
     private int xSize;
     private int zSize;
@@ -32,20 +39,36 @@ public class SpawnPillarField : MonoBehaviour
     private int spawnsSinceLastWait = 0;
 
 
+    private Vector3 pillarSpawnPos;
+    private float pillarHeight;
+    private int pillarWidth;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        if (pillarsSpawnDownward)
+        {
+            boxCastDir = Vector3.down;
+        }
+        else
+        {
+            boxCastDir = Vector3.up;
+        }
+
+
         combineMesh = GetComponent<CombineMesh>();
 
         xSize = Mathf.RoundToInt(transform.localScale.x - 2);
         zSize = Mathf.RoundToInt(transform.localScale.z - 2);
 
-        pos = transform.position;
+        basePosition = transform.position;
 
         //start at upper left hand corner -1/2xSize, -1/2zSize
-        xOffset = Mathf.RoundToInt(pos.x - ((xSize - 1) / 2));
-        zOffset = Mathf.RoundToInt(pos.z - ((zSize - 1) / 2));
+        xOffset = Mathf.RoundToInt(basePosition.x - ((xSize - 1) / 2));
+        zOffset = Mathf.RoundToInt(basePosition.z - ((zSize - 1) / 2));
 
         Random.InitState(transform.position.GetHashCode() % 100000);
 
@@ -59,44 +82,22 @@ public class SpawnPillarField : MonoBehaviour
         {
             for (int j = 0; j < zSize; j++)
             {
-                Vector3 spawnPos = new Vector3(xOffset + i, pos.y, zOffset + j);
+                pillarSpawnPos = new Vector3(xOffset + i, basePosition.y, zOffset + j);
 
                 //Random.InitState(spawnPos.GetHashCode() % 100000);
 
                 if (Random.Range(0f, 100f) < pillarSpawnChance)
                 {
-                    //generates a pillar height
-                    float pillarHeight = Random.Range(minHeight, maxHeight);
+                    SetPillarDimensions();
 
-                    pillarHeight = Mathf.RoundToInt(pillarHeight);
+                    BoxcastToAdjustHeight();
 
-                    //chance to double a pillar's height
-                    if (Random.Range(0f, 100f) < chanceOfDoubleHeightPillar)
+                    if (pillarHeight > 0)
                     {
-                        pillarHeight *= 2f;
+                        SetPillarYPosition();
+
+                        SpawnPillar();
                     }
-
-                    int pillarWidth = Random.Range(0, 5) * 2 + 1;
-
-
-                    RaycastHit hit;
-                    Vector3 halfExtents = new Vector3(pillarWidth / 2, 0.1f, pillarWidth / 2);
-
-                    //see if a pillar of the selected height would hit something
-                    if (Physics.BoxCast(spawnPos, halfExtents, Vector3.up, out hit, Quaternion.identity, pillarHeight))
-                    {
-                        pillarHeight = Mathf.RoundToInt(hit.point.y - pos.y);
-                    }
-
-                    spawnPos.y = (pos.y + 1) + ((pillarHeight - 1) / 2);
-
-                    Vector3 pillarScale = new Vector3(pillarWidth, pillarHeight, pillarWidth);
-
-                    Transform newPillar = Instantiate(pillar, spawnPos, Quaternion.identity).transform;
-
-                    newPillar.localScale = pillarScale;
-                    newPillar.parent = pillarFieldMeshes.transform;
-
 
                     spawnsSinceLastWait++;
 
@@ -110,7 +111,62 @@ public class SpawnPillarField : MonoBehaviour
         }
 
         combineMesh.Combine(pillarFieldMeshes);
+    }
 
+
+    void SetPillarDimensions()
+    {
+        //generates a pillar height
+        pillarHeight = Random.Range(minHeight, maxHeight);
+
+        pillarHeight = Mathf.RoundToInt(pillarHeight);
+
+        //chance to double a pillar's height
+        if (Random.Range(0f, 100f) < chanceOfDoubleHeightPillar)
+        {
+            pillarHeight *= 2f;
+        }
+
+        pillarWidth = Random.Range(0, 2) * 2 + 1;
+    }
+
+
+    void BoxcastToAdjustHeight()
+    {
+        RaycastHit hit;
+        halfExtents = new Vector3(pillarWidth / 2, 0.1f, pillarWidth / 2);
+
+        //see if a pillar of the selected height would hit something
+        if (Physics.BoxCast(pillarSpawnPos, halfExtents, boxCastDir, out hit, Quaternion.identity, pillarHeight))
+        {
+            pillarHeight = Mathf.RoundToInt(hit.point.y - basePosition.y);
+            pillarHeight = Mathf.Abs(pillarHeight);
+            pillarHeight -= 1;
+        }
+    }
+
+
+    void SetPillarYPosition()
+    {
+        if (pillarsSpawnDownward)
+        {
+            pillarSpawnPos.y = (basePosition.y - 1) - ((pillarHeight - 1) / 2);
+        }
+        else
+        {
+            pillarSpawnPos.y = (basePosition.y + 1) + ((pillarHeight - 1) / 2);
+        }
+    }
+
+
+    void SpawnPillar()
+    {
+        Vector3 pillarScale = new Vector3(pillarWidth, pillarHeight, pillarWidth);
+
+        Transform newPillar = Instantiate(pillar, pillarSpawnPos, Quaternion.identity).transform;
+
+        newPillar.localScale = pillarScale;
+        newPillar.parent = pillarFieldMeshes.transform;
     }
 
 }
