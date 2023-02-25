348ec0985cf3a70330f75f2345fa202706ab2e03
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2023-02-25 15:59:47 -0800

    Started work on a new 3d maze generator

---
+++ Fungivore Alpha/Assets/Scripts/MazeGenerator3D.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator3D : MonoBehaviour
+{
+    public int sizeX;
+    public int sizeY;
+    public int sizeZ;
+
+    public int cellSize;
+
+    public GameObject cellPrefab;
+
+    // Create the 3D array to store the state and directions for each cell
+    int[,,] mazeGrid;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mazeGrid = new int[sizeX, sizeY, sizeZ];
+
+        //overlap box each cell to see if it's obstructed, then mark it 0 for empty and 1 for obstructed
+        InitializeCells();
+
+        //spawn a block in each empty cell
+        SpawnCellPrefabs();
+
+    }
+
+
+    void InitializeCells()
+    {
+        for (int x = 0; x < sizeX; x++)
+        {
+            for (int y = 0; y < sizeY; y++)
+            {
+                for (int z = 0; z < sizeZ; z++)
+                {
+                    var cellPosX = transform.position.x + (x * cellSize);
+                    var cellPosY = transform.position.y + (y * cellSize);
+                    var cellPosZ = transform.position.z + (z * cellSize);
+
+                    if (SpaceIsEmpty(cellPosX, cellPosY, cellPosZ))
+                    {
+                        mazeGrid[x, y, z] = 0;
+                    }
+                    else
+                    {
+                        mazeGrid[x, y, z] = 1;
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    void SpawnCellPrefabs()
+    {
+        for (int x = 0; x < sizeX; x++)
+        {
+            for (int y = 0; y < sizeY; y++)
+            {
+                for (int z = 0; z < sizeZ; z++)
+                {
+                    if (mazeGrid[x, y, z] == 0) //spawn a block if the cell is marked 0 for empty
+                    {
+                        var cellPosX = transform.position.x + (x * cellSize);
+                        var cellPosY = transform.position.y + (y * cellSize);
+                        var cellPosZ = transform.position.z + (z * cellSize);
+
+                        Instantiate(cellPrefab, new Vector3(cellPosX, cellPosY, cellPosZ), Quaternion.identity);
+                    }
+                }
+            }
+        }
+    }
+
+
+    bool SpaceIsEmpty(float x, float y, float z)
+    {
+        Collider[] hitColliders = Physics.OverlapBox(new Vector3(x, y, z), new Vector3(cellSize, cellSize, cellSize) / 2 , Quaternion.identity);
+        if (hitColliders.Length < 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+
+}
