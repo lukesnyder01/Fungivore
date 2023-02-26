5c97d535a98cc5155e0898c0704fc0b507c29965
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2023-02-26 15:07:23 -0800

    Got 3d maze generator mostly working

--- Fungivore Alpha/Assets/Scripts/MazeGenerator3D.cs
+++ Fungivore Alpha/Assets/Scripts/MazeGenerator3D.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class MazeGenerator3D : MonoBehaviour
 {
-    private float timeBetweenSteps = 0.001f;
+    private float timeBetweenSteps = 0.0f;
 
     public int sizeX;
     public int sizeY;
     public int sizeZ;
 
     public int cellSize;
+
+    public float chanceToMoveInOpenDirection = 0.5f;
+    public float chanceToCloseExterior = 0.5f;
 
     public GameObject cellPrefab;
 
@@ -19,18 +22,39 @@ public class MazeGenerator3D : MonoBehaviour
     public Vector3Int startingCell = new Vector3Int(0, 0, 0);
 
 
+    //class to store the state and bitmask of each cell in the maze
+    public class MazeCell
+    {
+        public int cellState; // 0 for open, 1 for blocked, 2 for active with a maze cell
+
+        public int bitmask; // 6 digit bitmask to define the open sides of each active maze cell
+    }
+
+
     // Create the 3D array to store the state and directions for each cell
-    int[,,] mazeGrid;
+    MazeCell[,,] mazeGrid;
 
 
     // list of cells to 
-    Queue<Vector3Int> cellsToCreate = new Queue<Vector3Int>();
+    List<Vector3Int> cellsToCreate = new List<Vector3Int>();
+
+
+    // Define bit masks for each direction
+    int maskNorth = 1 << 0;
+    int maskEast = 1 << 1;
+    int maskSouth = 1 << 2;
+    int maskWest = 1 << 3;
+    int maskUp = 1 << 4;
+    int maskDown = 1 << 5;
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        mazeGrid = new int[sizeX, sizeY, sizeZ];
+
+
 
         //overlap box each cell to see if it's obstructed, then mark it 0 for empty and 1 for obstructed
         InitializeCells();
@@ -50,23 +74,27 @@ public class MazeGenerator3D : MonoBehaviour
 
     void InitializeCells()
     {
+        mazeGrid = new MazeCell[sizeX, sizeY, sizeZ]; //initialize the whole grid
+
         for (int x = 0; x < sizeX; x++)
         {
             for (int y = 0; y < sizeY; y++)
             {
                 for (int z = 0; z < sizeZ; z++)
                 {
+                    mazeGrid[x, y, z] = new MazeCell(); //initialize each cell on the grid
+
                     var cellPosX = transform.position.x + (x * cellSize);
                     var cellPosY = transform.position.y + (y * cellSize);
                     var cellPosZ = transform.position.z + (z * cellSize);
 
                     if (SpaceIsEmpty(cellPosX, cellPosY, cellPosZ))
                     {
-                        mazeGrid[x, y, z] = 0;
+                       mazeGrid[x, y, z].cellState = 0;
                     }
                     else
                     {
-                        mazeGrid[x, y, z] = 1;
+                       mazeGrid[x, y, z].cellState = 1;
                     }
                 }
             }
@@ -76,9 +104,9 @@ public class MazeGenerator3D : MonoBehaviour
 
     void PickStartingCell()
     {
-        if (mazeGrid[startingCell.x, startingCell.y, startingCell.z] == 0)
+        if (mazeGrid[startingCell.x, startingCell.y, startingCell.z].cellState == 0)
         {
-            cellsToCreate.Enqueue(startingCell);
+            cellsToCreate.Add(startingCell);
         }
         else
         {
@@ -86,7 +114,7 @@ public class MazeGenerator3D : MonoBehaviour
         }
 
         //turn on the starting cell in the mazeGrid
-        mazeGrid[startingCell.x, startingCell.y, startingCell.z] = 2;
+        mazeGrid[startingCell.x, startingCell.y, startingCell.z].cellState = 2;
     }
 
 
@@ -98,74 +126,68 @@ public class MazeGenerator3D : MonoBehaviour
 
             if (cellsToCreate.Count > 0) 
             {
-                //get the first cell in cellsToCheck
-                var cellPos = cellsToCreate.Dequeue();
+                //get a random cell to check
+                var selectedCell = Random.Range(0, cellsToCreate.Count);
+                var cellPos = cellsToCreate[selectedCell];
+                cellsToCreate.RemoveAt(selectedCell);
 
-                //instantiate a new maze cell prefab
-                Transform currentCell = NewPrefabAtPos(cellPos);
+                //instantiate a new maze cell prefab, we'll set the walls later
+                Transform currentCellPrefab = NewPrefabAtPos(cellPos);
+
 
                 var x = cellPos.x;
                 var y = cellPos.y;
                 var z = cellPos.z;
 
 
-
-                //check the cell to the north
-                if (z + 1 < sizeZ) //if we're not all the way to the edge
+                if (z < sizeZ - 1) //check to the north
                 {
-                    if (mazeGrid[x, y, z + 1] == 0) //the cell is empty
-                    {
-                        mazeGrid[x, y, z + 1] = 2; //mark the cell as containing a maze cell
+                    CheckDirection(new Vector3Int(x, y, z + 1), maskNorth, maskSouth, cellPos);
+                }
 
-                        cellsToCreate.Enqueue(new Vector3Int(x, y, z + 1)); //add cell to the queue
+
+                if (x < sizeX - 1) //check to the east
+                {
+                    CheckDirection(new Vector3Int(x + 1, y, z), maskEast, maskWest, cellPos);
+                }
+
+
+                if (z > 1) //check to the south
+                {
+                    CheckDirection(new Vector3Int(x, y, z - 1), maskSouth, maskNorth, cellPos);
+                }
+
+
+                if (x > 1) //check to the west
+                {
+                    CheckDirection(new Vector3Int(x - 1, y, z), maskWest, maskEast, cellPos);
+                }
+
+
+                if (y < sizeY - 1) //check above
+                {
+                    CheckDirection(new Vector3Int(x, y + 1, z), maskUp, maskDown, cellPos);
+                }
+
+
+                if (y > 1) //check below
+                {
+                    CheckDirection(new Vector3Int(x, y - 1, z), maskDown, maskUp, cellPos);
+                }
+
+
+                //sets the walls from the bitmask
+                for (int i = 0; i < 6; i++)
+                {
+                    if ((mazeGrid[x, y, z].bitmask & (1 << i)) != 0)
+                    {
+                        currentCellPrefab.GetChild(i).gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        currentCellPrefab.GetChild(i).gameObject.SetActive(false);
                     }
                 }
-
-
-                //check the cell to the east
-                if (x + 1 < sizeX) //if we're not all the way to the edge
-                {
-                    if (mazeGrid[x + 1, y, z] == 0) //the cell is empty
-                    {
-                        mazeGrid[x + 1, y, z] = 2; //mark the cell as containing a maze cell
-
-                        cellsToCreate.Enqueue(new Vector3Int(x + 1, y, z)); //add cell the the queue
-                    }
-                }
-
-
-                //check the cell above
-                if (y + 1 < sizeY) //if we're not all the way to the edge
-                {
-                    if (mazeGrid[x, y + 1, z] == 0) //the cell is empty
-                    {
-                        mazeGrid[x, y + 1, z] = 2; //mark the cell as containing a maze cell
-
-                        cellsToCreate.Enqueue(new Vector3Int(x, y + 1, z)); //add cell the the queue
-                    }
-                }
-
-
-
-
-
-                /*
-                // Check the cell to the north
-                if (x > 0 && mazeGrid[x - 1, y, z] == 1)
-                {
-                    mazeGrid[x, y, z] |= maskNorth;
-                    mazeGrid[x - 1, y, z] |= maskSouth;
-                }
-                */
-
-
-
-
-                //look at the adjacent cells to see which ones are open
-                //each open cell has a chance to get added to cellsToCheck and get turned on
-                //if it's open and added to cellsToCheck, it should get marked as having an open wall facing the current cell
-                //the current cell should also open its wall in the direction of the new cell
-
 
             }
             else 
@@ -173,18 +195,55 @@ public class MazeGenerator3D : MonoBehaviour
                 Debug.Log("No cells left to check, ending maze");
                 //end the maze
                 break;
-
             }
 
 
 
-            yield return new WaitForSeconds(timeBetweenSteps);
+            //yield return new WaitForSeconds(timeBetweenSteps);
+            yield return null;
+
         }
 
-        Debug.Log("Reached maximum maze size, ending maze");
         //end the maze
 
     }
+
+    void CheckDirection(Vector3Int cellCoords, int myMask, int adjacentMask, Vector3Int currentCellPos)
+    {
+        var cellToCheck = mazeGrid[cellCoords.x, cellCoords.y, cellCoords.z];
+
+
+        if (cellToCheck.cellState == 0) //if the cell is empty
+        {
+            // chance to add cell to open direction
+            if (Random.value < chanceToMoveInOpenDirection)
+            {
+                cellToCheck.cellState = 2; //mark the cell as containing a maze cell
+                cellsToCreate.Add(cellCoords); //add cell to the queue
+                mazeGrid[currentCellPos.x, currentCellPos.y, currentCellPos.z].bitmask |= myMask;
+                cellToCheck.bitmask |= adjacentMask;
+            }
+            else if (Random.value < chanceToCloseExterior)
+            {
+                mazeGrid[currentCellPos.x, currentCellPos.y, currentCellPos.z].bitmask &= ~myMask;
+            }
+
+
+        }
+        else if (cellToCheck.cellState == 1) //if the cell is blocked
+        {
+            if (Random.value < chanceToCloseExterior)
+            {
+                mazeGrid[currentCellPos.x, currentCellPos.y, currentCellPos.z].bitmask &= ~myMask;
+            }
+        }
+        else if (cellToCheck.cellState == 2) //if the cell is occupied by a maze cell
+        {
+
+        }
+    }
+
+
 
 
     Transform NewPrefabAtPos(Vector3Int pos)
@@ -197,17 +256,8 @@ public class MazeGenerator3D : MonoBehaviour
         Transform newCell = Instantiate(cellPrefab, new Vector3(cellPosX, cellPosY, cellPosZ), Quaternion.identity).transform;
 
         return newCell;
-
-        /*
-        for (int i = 0; i < newCell.childCount; i++)
-        {
-            if (Random.value > 0.8)
-            {
-                newCell.GetChild(i).gameObject.SetActive(true);
-            }
-        }
-        */
     }
+
 
 
     bool SpaceIsEmpty(float x, float y, float z)
@@ -232,7 +282,7 @@ public class MazeGenerator3D : MonoBehaviour
             {
                 for (int z = 0; z < sizeZ; z++)
                 {
-                    if (mazeGrid[x, y, z] == 0) //spawn a block if the cell is marked 0 for empty
+                    if (mazeGrid[x, y, z].cellState == 0) //spawn a block if the cell is marked 0 for empty
                     {
                         var cellPosX = transform.position.x + (x * cellSize);
                         var cellPosY = transform.position.y + (y * cellSize);
