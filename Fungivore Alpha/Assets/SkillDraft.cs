a6f366155e33b1fac3edf4afc6685301d32d78f8
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2023-03-24 12:13:02 -0700

    Further development of the stat upgrade drafting system, now randomly generates upgrade cards of different values to correspond to rarity

--- Fungivore Alpha/Assets/SkillDraft.cs
+++ Fungivore Alpha/Assets/SkillDraft.cs
@@ -1,36 +1,74 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class SkillDraft : MonoBehaviour
 {
-    public List<string> skillDeck = new List<string>();
 
-    public List<string> currentPool = new List<string>();
-
-
-    public int skillsPerDraft = 3;
+    public List<UpgradeCard> currentPool = new List<UpgradeCard>();
 
 
-
-    private List<int> skillValuePool = new List<int>();
-
+    int cardsPerDraft = 100;
 
 
-    private int legendaryCount = 1;
-    private int rareCount = 8;
-    private int uncommonCount = 32;
-    private int commonCount = 128;
+    private List<int> upgradeValues = new List<int>();
+
+
+    // will hold all of the possible stat modifiers in the game, even ones that are currently locked or have already been maxed out
+    private List<StatModifier> masterStatModList = new List<StatModifier>();
+
+
+    // will hold all of the stat mods that could currently show up on an upgrade card
+    // when a new stat mod is unlocked it gets added to this list and can be selected whenever an upgrade card is created
+    private List<StatModifier> statModPool = new List<StatModifier>();
+
+
+
+
+    private List<UpgradeCard> commonUpgradePool = new List<UpgradeCard>();
+
+
+
+    public class StatModifier
+    {
+        public readonly string statName;
+        public readonly int pointValue;
+        public readonly float changePerLevel;
+
+        public StatModifier(string _statName, int _pointValue, float _changePerLevel)
+        {
+            statName = _statName;
+            pointValue = _pointValue;
+            changePerLevel = _changePerLevel;
+        }
+    }
+
+
+    public class UpgradeCard
+    {
+        public int totalPointValue;
+
+        public List<StatModifier> statModifiers = new List<StatModifier>();
+    }
+
+
+
+
+
 
 
 
     void Awake()
     {
+        statModPool.Add(new StatModifier("Max Health", 1, 5f));
+        statModPool.Add(new StatModifier("Move Speed", 1, 0.2f));
+        statModPool.Add(new StatModifier("Spine Damage", 1, 1f));
 
-        InitializeSkillValuePool();
 
-        InitializeSkillDeck();
+        statModPool.Add(new StatModifier("Armor", 2, 1f));
 
+        statModPool.Add(new StatModifier("Double Jumps", 4, 1f));
+
+        InititalizeUpgradeValues();
     }
 
 
@@ -44,94 +82,104 @@ public class SkillDraft : MonoBehaviour
     }
 
 
-    void InitializeSkillValuePool()
+    void InititalizeUpgradeValues()
     {
+         int legendaryCount = 2;
+         int rareCount = 8;
+         int uncommonCount = 32;
+         int commonCount = 64;
+
+
         for (int i = 0; i < legendaryCount; i++)
         {
-            skillValuePool.Add(8);
+            upgradeValues.Add(8);
         }
 
         for (int i = 0; i < rareCount; i++)
         {
-            skillValuePool.Add(4);
+            upgradeValues.Add(4);
         }
 
         for (int i = 0; i < uncommonCount; i++)
         {
-            skillValuePool.Add(2);
+            upgradeValues.Add(2);
         }
 
         for (int i = 0; i < commonCount; i++)
         {
-            skillValuePool.Add(1);
+            upgradeValues.Add(1);
         }
     }
 
-
-    void InitializeSkillDeck()
-    {
-        skillDeck.Add("max HP +10");
-        skillDeck.Add("max HP +10");
-        skillDeck.Add("max HP +10");
-        skillDeck.Add("max HP +15");
-        skillDeck.Add("spine damage +1");
-        skillDeck.Add("spine damage +1");
-        skillDeck.Add("spine damage +2");
-        skillDeck.Add("armor +1");
-        skillDeck.Add("armor +1");
-        skillDeck.Add("armor +2");
-    }
 
 
     void StartNewDraft()
     {
-        Debug.Log("Starting new draft, shuffling...");
+        Debug.Log("Starting new draft...");
 
-        ShuffleSkillList();
 
-        Debug.Log("Shuffle completed, adding skills to pool...");
-
-        for(int i = 0; i < skillsPerDraft; i++)
+        for(int i = 0; i < cardsPerDraft; i++)
         {
-            AddSkillToDraftPool();
+            CreateNewUpgradeCard();
         }
 
     }
 
 
-    void AddSkillToDraftPool()
+    void CreateNewUpgradeCard()
     {
-        Debug.Log("Added skill worth " + NewSkillValue() + " points.");
+        //create a new card
+        UpgradeCard newCard = new UpgradeCard();
 
-        /*
-        int index = skillDeck.Count - 1;
-        currentPool.Add(skillDeck[index]);
-        Debug.Log("[[ " + skillDeck[index] + " ]] was added to draft pool.");
-        skillDeck.RemoveAt(index);
+        //give it a point value from the upgrade values list
+        newCard.totalPointValue = NewUpgradeValue();
 
-        */
-    }
+        var currentPointValue = 0;
 
+        var currentTries = 0;
 
-    int NewSkillValue()
-    {
-        return skillValuePool[Random.Range(0, skillValuePool.Count)];
-    }
-
-
-
-    void ShuffleSkillList()
-    {
-        var ts = skillDeck;
-        var count = ts.Count;
-        var last = count - 1;
-        for (var i = 0; i < last; ++i)
+        //randomly select a stat mod from the pool and add it if it doesn't make the upgrade card go over its point value
+        while (currentPointValue < newCard.totalPointValue)
         {
-            var r = UnityEngine.Random.Range(i, count);
-            var tmp = ts[i];
-            ts[i] = ts[r];
-            ts[r] = tmp;
+            //make sure we don't get stuck in an infinte loop if no remaining stat mod fits
+            currentTries++;
+
+            if (currentTries > 100)
+            {
+                return;
+            }
+
+            //pick a random mod from the mod pool
+            var potentialMod = statModPool[Random.Range(0, statModPool.Count)];
+
+            //if the mod doesn't cost too many points, add it to the upgrade card
+            if (potentialMod.pointValue + currentPointValue <= newCard.totalPointValue)
+            {
+                newCard.statModifiers.Add(potentialMod);
+                currentPointValue += potentialMod.pointValue;
+            }
         }
+
+        currentPool.Add(newCard);
+
+        Debug.Log("Card value = " + newCard.totalPointValue);
+
+        Debug.Log("┌──────────────────────────");
+
+        for (int i = 0; i < newCard.statModifiers.Count; i++)
+        {
+            Debug.Log("│ " + newCard.statModifiers[i].statName + " +" + newCard.statModifiers[i].changePerLevel);
+        }
+
+        Debug.Log("└──────────────────────────");
+
+
+    }
+
+
+    int NewUpgradeValue()
+    {
+        return upgradeValues[Random.Range(0, upgradeValues.Count)];
     }
 
 
