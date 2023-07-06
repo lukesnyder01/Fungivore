871f03479c24575598e238cceed98e86c70b4397
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2023-07-06 07:15:16 -0700

    Added turret enemies that shoot at player

---
+++ Fungivore Alpha/Assets/Scripts/Enemies/EnemyProjectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    private bool collidedWithPlayer = false;
+    public float damage;
+
+    public virtual void OnTriggerEnter(Collider other)
+    {
+
+            //Destroy(gameObject);
+
+
+        //Instantiate(deathEffect, transform.position, transform.rotation);
+        //FindObjectOfType<AudioManager>().Play("PlayerHurt");
+
+        if (other.gameObject.tag == "Player")
+        {
+            if (!collidedWithPlayer)
+            {
+                collidedWithPlayer = true;
+
+                FindObjectOfType<AudioManager>().Play("WraithSmash");
+                var playerStats = other.gameObject.GetComponent<PlayerStats>();
+                playerStats.ApplyDamage(damage);
+            }
+        }
+    }
+}
---
+++ Fungivore Alpha/Assets/Scripts/Enemies/TurretEnemy.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretEnemy : Enemy
+{
+    public float timeBetweenShots;
+    public float bulletSpeed;
+    private float shotTimer = 0;
+
+    public Rigidbody bulletPrefab;
+
+
+    public override void UpdateStateMachine()
+    {
+        PointAtPlayer();
+
+        shotTimer -= Time.deltaTime;
+
+        
+
+        switch (state)
+        {
+            default:
+
+            case State.Idle:
+
+                if (distanceFromPlayer < detectionRange)
+                {
+                    state = State.Hunt;
+                }
+
+                break;
+
+
+            case State.Hunt:
+
+                if (distanceFromPlayer > detectionRange)
+                {
+                    state = State.Idle;
+                }
+
+                if (CanSeePlayer())
+                {
+                    if (shotTimer <= 0)
+                    {
+                        shotTimer = timeBetweenShots;
+                        ShootAtPlayer();
+                    }
+                }
+
+                break;
+
+        }
+
+    }
+
+    public void ShootAtPlayer()
+    {
+        Debug.Log("shot at player");
+        var newBullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        newBullet.velocity = transform.forward * bulletSpeed;
+
+    }
+
+
+
+}
