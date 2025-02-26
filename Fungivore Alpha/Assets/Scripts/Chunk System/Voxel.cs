cfe10ab0fdb3a78d6e84ecacc0500db84cfd7f43
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2025-02-26 07:54:44 -0800

    voxel: add new voxel type so we can test texture atlas

--- Fungivore Alpha/Assets/Scripts/Chunk System/Voxel.cs
+++ Fungivore Alpha/Assets/Scripts/Chunk System/Voxel.cs
@@ -9,6 +9,7 @@ public struct Voxel
     {
         public const byte Air = 0;
         public const byte Stone = 1;
+        public const byte Stone02 = 2;
         // Easy to add more:
         // public const byte Dirt = 2;
         // public const byte Grass = 3;
