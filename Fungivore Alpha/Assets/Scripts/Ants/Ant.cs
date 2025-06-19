98dbfae42bfc80d63ad5e1568354f3df73d39306
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2025-06-19 16:56:47 -0700

    shader: #47 create texture array and test with new voxel shader

--- Fungivore Alpha/Assets/Scripts/Ants/Ant.cs
+++ Fungivore Alpha/Assets/Scripts/Ants/Ant.cs
@@ -47,6 +47,7 @@ public class Ant
     public bool BlockIsEmpty(Vector3 position)
     {
         currentChunk = World.Instance.GetChunkAt(position);
+
         if (currentChunk != null && currentChunk.GetBlockGlobal(position) == Voxel.Type.Air)
         {
             // Also check a overlap box here for entities
